d383e75f83c64c0f32d1a3ff25325f833a69ed0e
sneg55 <nikita@sawinyh.ru>
2026-03-27 10:53:32 -0400

    fix: resolve duplicate Alembic revision ID, apply email verification columns

--- alembic/versions/seed_model_routing_defaults.py
+++ alembic/versions/seed_model_routing_defaults.py
@@ -1,7 +1,7 @@
 """seed model routing defaults
 
-Revision ID: a1b2c3d4e5f6
-Revises: 2b8217a29c38
+Revision ID: b2c3d4e5f6a7
+Revises: a1b2c3d4e5f6
 Create Date: 2026-03-26 22:00:00.000000
 
 """
@@ -11,8 +11,8 @@ from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, Sequence[str], None] = '2b8217a29c38'
+revision: str = 'b2c3d4e5f6a7'
+down_revision: Union[str, Sequence[str], None] = 'a1b2c3d4e5f6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
